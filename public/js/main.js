55aa5c9033c5ed570e6afdb22718a25eee885829
rnormando <42142498+rnormando@users.noreply.github.com>
2018-10-19 08:43:17 -0300

    Finalizado
    * Campo mensagens feito

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,11 @@
       $('.sidenav').sidenav();
       $('.collapsible').collapsible();
       $('.tabs').tabs();
+      $(document).click((event) => {
+        if (event.target.id === 'icon_prefix_input') {
+          $('#icon_prefix').toggleClass('active');
+          return;
+        }
+        $('#icon_prefix').toggleClass('active');
+      });
     });
